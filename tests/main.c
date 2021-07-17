9dd0b67542201022c18b98b127e80462829b70a2
madebypixel02 <madebypixel02@gmail.com>
2021-07-17 20:34:20 +0200

    Added fun test reading from two fd at a time

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 09:14:39 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/17 20:19:48 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/17 20:34:07 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,14 @@ int	main(void)
 	char	*line[4096];
 
 	i = 1;
-	//fd_1 = open("tests/J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt", O_RDONLY);
-	fd_1 = open("tests/test.txt", O_RDONLY);
-	fd_2 = open("tests/test2.txt", O_RDONLY);
+	fd_1 = open("tests/J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt", O_RDONLY);
+	//fd_1 = open("tests/test.txt", O_RDONLY);
+	//fd_2 = open("tests/test2.txt", O_RDONLY);
 	//fd = open("tests/empty.txt", O_RDONLY);
 	//fd_1 = open("tests/41_no_nl.txt", O_RDONLY);
 	//fd = open("tests/big_line_no_nl.txt", O_RDONLY);
 	//fd_2 = open("tests/basic_oneline.txt", O_RDONLY);
-	//fd_2 = open("tests/the-king-james-bible.txt", O_RDONLY);
+	fd_2 = open("tests/the-king-james-bible.txt", O_RDONLY);
 	while (1)
 	{
 		line[fd_1] = get_next_line(fd_1);
