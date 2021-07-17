6089be08e9b3095ce4932dd34b1d82c4c36bbd5a
madebypixel02 <madebypixel02@gmail.com>
2021-07-17 20:38:50 +0200

    Added fun test reading from two fd at a time

--- tests/main.c
+++ tests/main.c
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/12 09:14:39 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/07/17 20:34:07 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/07/17 20:38:42 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,9 @@ int	main(void)
 	fd_1 = open("tests/J. K. Rowling - Harry Potter 1 - Sorcerer's Stone.txt", O_RDONLY);
 	//fd_1 = open("tests/test.txt", O_RDONLY);
 	//fd_2 = open("tests/test2.txt", O_RDONLY);
-	//fd = open("tests/empty.txt", O_RDONLY);
+	//fd_1 = open("tests/empty.txt", O_RDONLY);
 	//fd_1 = open("tests/41_no_nl.txt", O_RDONLY);
-	//fd = open("tests/big_line_no_nl.txt", O_RDONLY);
+	//fd_2 = open("tests/big_line_no_nl.txt", O_RDONLY);
 	//fd_2 = open("tests/basic_oneline.txt", O_RDONLY);
 	fd_2 = open("tests/the-king-james-bible.txt", O_RDONLY);
 	while (1)
