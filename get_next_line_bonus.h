a7cbffcafba7c0e11e374ef2733eec97e978d275
madebypixel02 <madebypixel02@gmail.com>
2021-09-05 20:26:50 +0200

    Commented functions in .h files

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: aperez-b <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 10:42:19 by aperez-b          #+#    #+#             */
-/*   Updated: 2021/08/29 19:43:26 by aperez-b         ###   ########.fr       */
+/*   Updated: 2021/09/05 20:25:24 by aperez-b         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,14 +24,31 @@
 
 # endif
 
+/* Retrieves next line from given file descriptor */
 char	*get_next_line(int fd);
+
+/* Returns length of a string */
 size_t	gnl_strlen(const char *s);
+
+/* Retrieves index of first appearance of i in s, or -1 */
 int		gnl_strchr_i(const char *s, int c);
+
+/* Copies chars from src to dst ensuring null-termination */
 size_t	gnl_strlcpy(char *dst, const char *src, size_t size);
+
+/* Allocates enough memory to make a copy of s from start, copying len bytes */
 char	*gnl_substr(char const *s, unsigned int start, size_t len);
+
+/* Concatenates two strings, ensuring null-termination */
 size_t	gnl_strlcat(char *dst, const char *src, size_t size);
+
+/* Removes line from the static variable buf */
 char	*gnl_shrink_buffer(char *buf, char *line);
+
+/* Performs a new read and adds it to static variable buf */
 char	*gnl_expand_buffer(char *buf, int fd);
+
+/* Performs a new read and returns read string */
 char	*gnl_newread(int fd);
 
 #endif
